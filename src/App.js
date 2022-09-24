cf94787901dbc2b810e48b027a31c36ae6c7dd19
Abu Siam <siamcse01@gmail.com>
2022-09-24 16:14:32 +0600

    Navbar added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainComponents from './components/MainComponents';
+
+function App() {
+  return (
+    <div className="App">
+      <MainComponents></MainComponents>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainComponents.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Body from './body/Body';
+import Footer from './footer/Footer';
+import Header from './header/Header';
+
+const MainComponents = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Body></Body>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default MainComponents;
