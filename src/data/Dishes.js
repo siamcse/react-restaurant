72e38d6576b6069f05d4a8d048eb32e10656b110
Abu Siam <siamcse01@gmail.com>
2022-09-24 18:00:02 +0600

    dish added

---
+++ src/data/Dishes.js
@@ -0,0 +1,76 @@
+const dishes = [
+    {
+        "id": "the-gramercy-tavern-burger-4-pack",
+        "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/137148/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-15.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+        "name": "Gramercy Tavern",
+        "dsc": "The Gramercy Tavern Burger - 4 Pack",
+        "price": 99,
+        "rate": 5,
+        "country": "New York, NY"
+    },
+    {
+    "id": "shake-shack-shackburger-8-pack",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134862/shake-shack-shackburger-8-pack.973a5e26836ea86d7e86a327becea2b0.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Shake Shack",
+    "dsc": "Shake Shack ShackBurger® – 8 Pack",
+    "price": 49,
+    "rate": 5,
+    "country": "New York, NY"
+    },
+    {
+    "id": "gotts-cheeseburger-kit-for-4",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/132933/gotts-complete-cheeseburger-kit-for-4.7bdc74104b193427b3fe6eae39e05b5e.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Gott's Roadside",
+    "dsc": "Gott's Complete Cheeseburger Kit for 4",
+    "price": 99,
+    "rate": 5,
+    "country": "St. Helena, CA"
+    },
+    {
+    "id": "le-big-matt-kit-for-6",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/131436/le-big-matt-kit-for-6.1ddae6e382bb3218eeb0fd5247de115a.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Emmy Squared",
+    "dsc": "Le Big Matt Burger Kit for 6",
+    "price": 99,
+    "rate": 5,
+    "country": "Brooklyn, NY"
+    },
+    {
+    "id": "shake-shack-shackburger-16-pack",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/134022/shake-shack-shackburger-16-pack.316f8b09144db65931ea29e34869287a.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Shake Shack",
+    "dsc": "Shake Shack Shackburger® – 16 Pack",
+    "price": 89,
+    "rate": 4,
+    "country": "New York, NY"
+    },
+    {
+    "id": "wagyu-burger-patties-12-pack",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/107019/wagyu-burger-patties-12-pack.6116f4cd648dee20651f99e21e7d758b.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Westholme Wagyu",
+    "dsc": "Wagyu Burger Patties - 12 Pack",
+    "price": 129,
+    "rate": 5,
+    "country": "Queensland, Australia"
+    },
+    {
+    "id": "21-usda-prime-burgers-pack-of-18-8oz-each",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/133009/usda-prime-burgers-pack-of-18-8oz-each.274c67f15aa1c0b210dbf51801706670.png?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Peter Luger Steak House",
+    "dsc": "USDA Prime Burgers - Pack of 18 (8oz each)",
+    "price": 175.95,
+    "rate": 4,
+    "country": "Brooklyn, NY"
+    },
+    {
+    "id": "burger-bomb-kit-for-6",
+    "img": "https://goldbelly.imgix.net/uploads/showcase_media_asset/image/133829/burger-bomb-kit-for-6.b0430200cfc153c1c15c7997236a6152.jpg?ixlib=react-9.0.2&auto=format&ar=1%3A1",
+    "name": "Old Homestead Steakhouse",
+    "dsc": "Burger Bomb Kit for 6",
+    "price": 129,
+    "rate": 5,
+    "country": "New York, NY"
+    }
+];
+const DISHES = dishes.map(dish => (dish));
+export default DISHES;
